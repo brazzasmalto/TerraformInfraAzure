c82d6ae7c9e77659a9241b4c5da86ab512d5a458
brazzasmalto <97316407+brazzasmalto@users.noreply.github.com>
2022-01-09 23:45:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 #In Azure, all infrastructure elements such as virtual machines, storage, and our Kubernetes cluster need to be attached to a resource group.
 
-resource "azurerm_resource_group" "brakin-aks-rg" {
+resource "azurerm_resource_group" "aks-rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_role_assignment" "brakin-role_acrpull" {
+resource "azurerm_role_assignment" "role_acrpull" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_container_registry" "brakin-acr" {
+resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.brakin-aks-rg.name
+  resource_group_name = azurerm_resource_group.aks-rg.name
   location            = var.location
   sku                 = "Standard"
   admin_enabled       = false
 }
 
-resource "azurerm_kubernetes_cluster" "brakin-aks" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.brakin-aks-rg.name
+  resource_group_name = azurerm_resource_group.aks-rg.name
   dns_prefix          = var.cluster_name
 
   default_node_pool {
